f9292b1f1b90b794aa11b805c11b8f19fe2ec169
kastashonok <konstantin.astashonok@gmail.com>
2022-12-12 19:44:50 +0100

    it-kamasutra-89-lessons

--- src/redux/reducers/profile-reducer.test.js
+++ src/redux/reducers/profile-reducer.test.js
@@ -1,18 +1,24 @@
 import profileReducer, {addPostActionCreator} from "./profile-reducer";
 import React from "react";
 
+let state = {
+	postData: [
+		{id: 1, message: 'Hi, how are you?', likeCount: 12},
+		{id: 2, message: 'It is my firs post.', likeCount: 11},
+		{id: 3, message: 'It is my firs post1.', likeCount: 13}
+	]
+};
+
 it('length of posts should be incremented', () => {
 	let addPostAC = addPostActionCreator('it-kamasutra.com');
-
-	let state = {
-		postData: [
-			{id: 1, message: 'Hi, how are you?', likeCount: 12},
-			{id: 2, message: 'It is my firs post.', likeCount: 11},
-			{id: 3, message: 'It is my firs post1.', likeCount: 13}
-		]
-	};
-
 	let newState = profileReducer(state, addPostAC);
 
 	expect(newState.postData.length).toBe(4);
+})
+
+it('length of posts should be incremented', () => {
+	let addPostAC = addPostActionCreator('it-kamasutra.com');
+	let newState = profileReducer(state, addPostAC);
+
+	expect(newState.postData[3].message).toBe('it-kamasutra.com');
 })
